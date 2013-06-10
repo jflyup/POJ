f3e9372a1a129e4486520e9d0c2ab66fbf5cdbf5
Jason J <Jason@Jasons-MacBook-Air.local>
2013-06-10 22:09:45 +0800

    reduce the  running time from O(n ^ 2) to O(n log n)

--- poj2533.c
+++ poj2533.c
@@ -1,28 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int LOS(int* a, int size)
+int binary_search(int *arr, int size, int x)
 {
-	int* value_arr = (int*)calloc(sizeof(int), size);
+	int left = 0;
+	int right = size - 1;
+	int mid;
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+		if(arr[mid] <= x)
+			left = mid + 1;
+		else if(arr[mid] > x)
+			right = mid - 1;
+	}
+	return left;
+}
 
-	int i, j;
+int LIS(int* a, int size)
+{
+	//M[j] stores the value of the smallest value a[k] such that there is an increasing subsequence of length j ending at a[k] on the range
+	int* M = (int*)calloc(sizeof(int), size);
+	M[0] = a[0];
+	int len = 1;
+	int i;
 	for(i = 1; i < size; ++i)
 	{
-		for(j = i - 1; j >= 0 ; --j)
-			if(a[i] > a[j])
-			{
-				int temp = value_arr[j] + 1;
-				if(temp > value_arr[i])
-					value_arr[i] = temp;
-			}
-
+		if(a[i] > M[len - 1])
+		{
+			M[len++] = a[i];
+		}
+		else
+		{
+			int pos = binary_search(M, len, a[i]);
+			M[pos] = a[i];
+		}
 	}
-	int result = value_arr[0];
-	for(i = 1; i < size; ++i)
-		if(value_arr[i] > result)
-			result = value_arr[i];
-	free(value_arr);
-	return result + 1;
+	free(M);
+	return len;
 }
 
 int main()
@@ -33,7 +48,7 @@ int main()
 	int i;
 	for(i = 0; i < size; ++i)
 		scanf("%d", &sequence[i]);
-	printf("%d\n", LOS(sequence, size) );
+	printf("%d\n", LIS(sequence, size) );
 	free(sequence);
 	return 0;
 }
