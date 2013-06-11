d32dd3fb7db184c912b25b4c827d16d9af776d0e
Jason J <Jason@Jasons-MacBook-Air.local>
2013-06-11 21:25:29 +0800

    reduce running time to O(n). Accepted.
    generate 10000 random numbers between -100 and 100 for testing in
    python.
    for i in range(1, 10000):
        random.randint(-100, 100)

--- 2479.c
+++ 2479.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <limits.h>
 
-#define LOG_MAX 16
-
 int max(int a, int b)
 {
 	return a > b ? a : b;
@@ -35,38 +33,6 @@ void reverse_array(int *a, int size)
 	}
 }
 
-/*
- * RMQ: Range Minimum/Maximum Query
- * dp[i][j] represent the min/max value in the subarray starting at i haveing length 2^j.
- * dp[i][0] = a[i]
- * dp[i][j] = max(dp[i][j - 1], dp[i + 2 ^ (j - 1)][j - 1])
- */
-
-void init_rmq(int *a, int len, int **dp)
-{
-	int i, j;
-	for(i = 0; i < len; ++i)
-	{
-		dp[i][0] = a[i];
-	}
-	for(j = 1; (1<<j) <= len; ++j)
-		for(i = 0; i + (1 << j) - 1 < len; i++)
-		{
-			dp[i][j] = max(dp[i][j - 1], dp[i + (1 << (j - 1))][j - 1]);
-		}
-}
-
-int rmq(int **dp, int begin, int end)
-{
-	int distance = end - begin;
-	int i;
-	for(i = 0; (1 << i) < distance; ++i);
-	if(i)
-		--i;
-	return max(dp[begin][i], dp[end - (1 << i) + 1][i]);
-
-}
-
 int main()
 {
 	int cases;
@@ -86,14 +52,13 @@ int main()
 		max_subarray(array, size, max_ending_here);
 
 		reverse_array(array, size);
-		int *reverse_max_ending_here =(int*)malloc(size * sizeof(int)); 
+		int *reverse_max_ending_here = (int*)malloc(size * sizeof(int)); 
 		max_subarray(array, size, reverse_max_ending_here);
 
-		int **dp = (int **)malloc(sizeof(int*) * size);
-		for(j = 0; j < size; ++j)
-			dp[j] = (int*)malloc(sizeof(int) * LOG_MAX);
-
-		init_rmq(reverse_max_ending_here, size, dp);
+		int *reverse_max_so_far = (int*)malloc(size * sizeof(int)); 
+		reverse_max_so_far[0] = reverse_max_ending_here[0];
+		for(j = 1; j < size; ++j)
+			reverse_max_so_far[j] = max(reverse_max_ending_here[j], reverse_max_so_far[j - 1]);
 
 		int maximum_sum = INT_MIN;
 		int first_max_subarray = max_ending_here[0];
@@ -101,7 +66,7 @@ int main()
 		{
 			if(max_ending_here[j] > first_max_subarray)
 				first_max_subarray = max_ending_here[j];
-			int sum = first_max_subarray + rmq(dp, 0, size - j - 2);
+			int sum = first_max_subarray + reverse_max_so_far[size - j - 2];
 			if(sum > maximum_sum)
 				maximum_sum = sum;
 		}
@@ -109,9 +74,7 @@ int main()
 		free(array);
 		free(max_ending_here);
 		free(reverse_max_ending_here);
-		for(j = 0; j < size; ++j)
-			free(dp[j]);
-		free(dp);
+		free(reverse_max_so_far);
 	}
 	return 0;
 }
