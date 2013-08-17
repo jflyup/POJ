0daf48c1a4cd44fa8057bfcfd3abc5974bee980c
Jason J <Jason@Jasons-MacBook-Air.local>
2013-08-17 10:48:58 +0800

    replace the recursive merge sort implementation with an interative one.

--- 2299.c
+++ 2299.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+
 //given at most 500,000 numbers, so the number of swap operations is less than 500000*499999/2
 long long g_swap_count;
 
@@ -8,13 +10,13 @@ void merge(int *a, int s, int m, int e)
 {
 	int p1 = s;
 	int p2 = m + 1;
-	 int * merged = (int*)malloc(sizeof(int) * (e - s + 1));
-	 int p = 0;
+	int * merged = (int*)malloc(sizeof(int) * (e - s + 1));
+	int p = 0;
 	while(p1 < m + 1 && p2 < e + 1)
 	{
 		if(a[p1] > a[p2])
 		{
-			 merged[p] = a[p2];
+			merged[p] = a[p2];
 			p2++;
 			g_swap_count += (m + 1 - p1);
 		}
@@ -43,37 +45,33 @@ void merge(int *a, int s, int m, int e)
 	 free(merged);
 }
 
-void merge_sort(int *a, int s, int e)
+void merge_sort(int *a, int n)
 {
-	if(s < e)
+	int width;
+	for(width = 1; width < n; width = 2 * width)
 	{
-		int mid = (s + e) / 2;
-		merge_sort(a, s, mid);
-		merge_sort(a, mid + 1, e);
-		merge(a, s, mid, e);
+		int i;
+		for(i = 0; i < n; i = i + 2 * width)
+		{
+			merge(a, i, min(i + width - 1, n - 1), min(i + 2 * width - 1, n - 1) );
+		}
 	}
 }
 
 int main()
 {
 	int size;
-	while(1)
+	while(scanf("%d", &size), size > 0)
 	{
-		scanf("%d", &size);
-		if(size > 0)
-		{
-			g_swap_count = 0;
-			int *array = (int *)malloc(sizeof(int) * size);
-			int i;
-			for(i = 0; i < size; ++i)
-				scanf("%d", &array[i]);
+		g_swap_count = 0;
+		int *array = (int *)malloc(sizeof(int) * size);
+		int i;
+		for(i = 0; i < size; ++i)
+			scanf("%d", &array[i]);
 
-			merge_sort(array, 0, size - 1);
-			free(array);
-			printf("%lld\n", g_swap_count);
-		}
-		else
-			break;
+		merge_sort(array, size);
+		free(array);
+		printf("%lld\n", g_swap_count);
 	}
 	return 0;
 }
