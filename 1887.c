45d82e5188614fd9c1189724352fc4b226d1247a
Jason J <Jason@Jasons-MacBook-Air.local>
2013-08-17 14:48:48 +0800

    longest non-increasing sequence

---
+++ 1887.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NMAX 10240
+
+/**
+* the array is sorted in descending order.
+* return the index of the first element that's not greater than the target element
+**/
+int binary_search(int *a, int value, int start, int end)
+{
+	while(start <= end)
+	{
+		int mid = (start + end) / 2;
+		if(a[mid] > value)
+		{
+			start = mid + 1;
+		}
+		else
+		{
+			end = mid - 1;
+		}
+	}
+
+	return start;
+}
+
+int LDS(int *a, int size)
+{
+	int i;
+	int *M = (int *)malloc(sizeof(int) * size);
+	M[0] = a[0];
+	int len = 0;
+	for(i = 1; i < size; ++i)
+	{
+		if(a[i] <= M[len])
+		{
+			M[++len] = a[i];
+		}
+		else
+		{
+			int pos = binary_search(M, a[i], 0, len);
+			M[pos] = a[i];
+		}
+
+	}
+	free(M);
+	return len + 1;
+}
+
+int main()
+{
+	int heights[NMAX];
+	int test_number = 0;
+	int size;
+	while(scanf("%d", &heights[0]), heights[0] != -1)
+	{
+		size = 1;
+		test_number++;
+		while(scanf("%d", &heights[size]), heights[size++] != -1);
+		printf("Test #%d:\n  maximum possible interceptions: %d\n\n", test_number, LDS(heights, size - 1) );
+	}
+
+	return 0;
+};
